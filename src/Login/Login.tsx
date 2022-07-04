21e7a398028c711f19c70acbe8b7d49bb162546f
naimulislamshakil <ms0413354@gmail.com>
2022-07-04 15:01:01 +0600

    login

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -1,10 +1,29 @@
 import React from "react";
+import auth from "../Firebase.config";
 import "./Login.css";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import Loading from "../Loading";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const [signInWithGoogle, user, loading] = useSignInWithGoogle(auth);
+
+  const navigate = useNavigate();
+
+  if (loading) {
+    return <Loading></Loading>;
+  }
+
+  if (user) {
+    navigate("/");
+  }
   return (
     <div className=" e">
-      <button type={"button"} className="btn btn-info b">
+      <button
+        onClick={() => signInWithGoogle()}
+        type={"button"}
+        className="btn btn-info b"
+      >
         Login With Google
       </button>
       <button type={"button"} className="btn btn-info b">
